ff2af2af0c6a3af69a02f92035ed5e757e2f5e6b
Megan Edwards <megmyfedwards@gmail.com>
2019-05-24 10:01:13 +0200

    Get character details by props

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -3,10 +3,21 @@ import React from 'react';
 class CharacterDetails extends React.Component {
   render() { 
     const {match, characterData} = this.props;
+    const id = parseInt(match.params.id);
+    const filteredCharacterData = characterData.find(item => item.id === id)
+
     return ( 
       <React.Fragment>
-        <p>Hello I'm a character!</p>
-        <p>My id is{match.params.id}</p>
+        {filteredCharacterData ?
+          <div className="details">
+            <p>Hello I'm {filteredCharacterData.name}!</p>
+            <p>My id is{id}</p>
+          </div>
+        :
+        <p>No tengo datos hasta ahora!</p>
+
+        }
+
       </React.Fragment>
     );
   }
